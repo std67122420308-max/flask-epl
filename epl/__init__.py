ff0200ad1a9227b1ca6e349313d8873520498231
std67122420308-max <std.67122420308@ubru.ac.th.>
2026-02-03 12:29:38 +0700

    เพิ่ม

--- epl/__init__.py
+++ epl/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///epl.sqlite'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from epl import models, routes
